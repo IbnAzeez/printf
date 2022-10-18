d4ba01c7c9626032bb053c300fcfb1f361e89028
IbnAzeez <hayzed003@gmail.com>
2022-10-18 15:56:22 -0700

    Random functions

--- print_rand.c
+++ print_rand.c
@@ -1,6 +1,48 @@
 #include "main.h"
 
 /**
+ * print_bigS - Non printable characters
+ * (0 < ASCII value < 32 or >= 127) are
+ * printed this way: \x, followed by the ASCII code
+ * value in hexadecimal (upper case - always 2 characters)
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct flags that determines
+ * if a flag is passed to _printf
+ * Return: number of char printed
+ */
+int print_bigS(va_list l, flags_t *f)
+{
+	int i, count = 0;
+	char *res;
+	char *s = va_arg(l, char *);
+
+	(void)f;
+	if (!s)
+		return (_puts("(null)"));
+
+	i = 0;
+
+	while (s[i])
+	{
+		if (s[i] > 0 && (s[i] < 32 || s[i] >= 127))
+		{
+			_puts("\\x");
+			count += 2;
+			res = convert(s[i], 16, 0);
+			if (!res[1])
+				count += _putchar('0');
+			count += _puts(res);
+		}
+		else
+			count += _putchar(s[i]);
+		i++;
+	}
+
+	return (count);
+}
+
+
+/**
  * print_rev - prints a string in reverse
  * @l: argument from _printf
  * @f: pointer to the struct flags that determines
