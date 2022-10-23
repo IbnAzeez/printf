2d03ccb126fee89d9d8f5d2b062a8ab26b7fa9b9
IbnAzeez <hayzed003@gmail.com>
2022-10-23 04:49:30 -0700

    Rot13

--- print_rot.c
+++ print_rot.c
@@ -16,7 +16,7 @@ int print_rot13(va_list l, flags_t *f)
 
 	(void)f;
 
-	for (j = 0; str[j] j++)
+	for (j = 0; str[j]; j++)
 	{
 		if (str[j] < 'A' || (str[j] > 'Z' && str[j] < 'a') || str[j] > 'z')
 			_putchar(str[j]);
